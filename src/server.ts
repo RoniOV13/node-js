cc18b2451fb39340c19e8c67df3ab7fb1c2a34b4
RoniOV13 <ovsepyan.vera@bk.ru>
2020-10-30 15:12:30 +0300

    comm

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { createServer } from 'http'
 import { readFile, access } from 'fs/promises'
-import { readFile as oldReadFile, readdir as oldReadDir, existsSync, readdirSync, lstatSync, truncate, statSync, Dir, opendirSync } from 'fs'
+import { readFile as oldReadFile, readdir as oldReadDir,exists, existsSync, readdirSync, lstatSync, truncate, statSync, Dir, opendirSync } from 'fs'
 const pug = require('pug');
+const fs = require('fs');
+import {join, extname} from 'path'
 
 const PUBLIC = process.cwd() + '/public'
 
@@ -48,24 +50,84 @@ createServer(async (req, res) => {
 
   await search(req.url, PUBLIC)
 
-  const compiledFunction = pug.compileFile('index1.pug')
-  console.log(compiledFunction({
+ 
+  // let filePath = '.' + req.url;
+  //   if (filePath == './')
+  //       filePath = './index.html';
+
+  //   let extname_ = extname(filePath);
+  //   let contentType = 'text/html';
+  //   switch (extname_) {
+  //       case '.js':
+  //           contentType = 'text/javascript';
+  //           break;
+  //       case '.css':
+  //           contentType = 'text/css';
+  //           break;
+  //       case '.json':
+  //           contentType = 'application/json';
+  //           break;
+  //       case '.png':
+  //           contentType = 'image/png';
+  //           break;      
+  //       case '.jpg':
+  //           contentType = 'image/jpg';
+  //           break;
+  //       case '.wav':
+  //           contentType = 'audio/wav';
+  //           break;
+  //           case '.woff':
+  //           contentType = 'font/woff';
+  //           break;
+  //       case '.woff2':
+  //           contentType = 'font/woff2';
+  //           break;      
+  //       case '.svg':
+  //           contentType = 'image/svg+xml';
+  //           break;
+  //       case '.gif': 
+  //           contentType = 'image/gif';
+  //           break;
+  //   }
+
+  //   fs.readFile(process.cwd() + '/public/path/'+ req.url, function(error, content) {
+  //       if (error) {
+  //           if(error.code == 'ENOENT'){
+  //               fs.readFile('./404.html', function(error, content) {
+  //                   res.writeHead(200, { 'Content-Type': contentType });
+  //                   res.end(content, 'utf-8');
+  //               });
+  //           }
+  //           else {
+  //               res.writeHead(500);
+  //               res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+  //               res.end(); 
+  //           }
+  //       }
+  //       else {
+  //           res.writeHead(200, { 'Content-Type': contentType });
+  //           res.end(content, 'utf-8');
+  //       }
+  //   });
+
+  const compiledFunction = pug.compileFile(process.cwd() + '/src/index1.pug')
+  const body = compiledFunction({
     local: 'Batman'
-  }));
+  });
+
+  res
+    .writeHead(200, {
+      'Content-Length': Buffer.byteLength(body),
+      'Content-Type': 'text/html'
+    })
+    .end(body);
 
 
-  // let fn = pug.compileFile('index1.pug');
-  // let html = fn({ local: 'hello world' });
+  //   const json = JSON.stringify({ a: 2 });
+  //   res.writeHead(200, {'content-type':'application/json', 'content-length':Buffer.byteLength(json)}); 
+  //  res.end(json);
 
-
-  //  html = pug.render('string of pug');
-
-
-  // pug.renderFile('index1.pug', function(err, html) {
-  //pug.send(html);
-  //});
-
- // res.end()
+  // res.end()
 
 }).listen(5000, 'localhost', () => {
   console.log('server listen on http://localhost:5000/')
